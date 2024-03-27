d7aacb421008416d3658f10e4c19af570813aff3
Abner-Wilhelm <abnerwilhelm@gmail.com>
2024-03-26 22:18:49 -0400

    SHOPPING WOO
    added item connecting to player
    added checkout

--- Assets/Scripts/ItemScripts/podiumdisplay.cs
+++ Assets/Scripts/ItemScripts/podiumdisplay.cs
@@ -9,15 +9,45 @@ public class podiumdisplay : MonoBehaviour
     public Database itemDatabase;
     public SpriteRenderer displaySprite;
     public float itemScale = 4;
+    private bool followPlayer = false;
+    private bool holding = false;
+    public Transform objectToFollow;
+    public Vector3 offset;
    
     public void Start()
     {
         randomDisplay();
+        holding = false;
     }
 
     public void Update()
     {
+        if (followPlayer == true)
+        {
+            transform.position = objectToFollow.position + offset;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "shop")
+        {
+            Debug.Log("follow player disabled");
+            followPlayer = false;
+            holding = false;
+            displaySprite.enabled = false;
+            return;
+        }
+
+        if (other.tag == "Player" && holding == false)
+            {
+            Debug.Log("Space pressed");
+            followPlayer = true;
+            holding = true;
+            return;
+            }
        
+
     }
 
     void randomDisplay()
