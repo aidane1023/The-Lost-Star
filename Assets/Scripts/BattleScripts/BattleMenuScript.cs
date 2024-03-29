17918510c9ea41043bef8e975ea8db9444cb4a31
SolidZan <zanman263@gmail.com>
2024-03-29 00:25:17 -0400

    Added Descriptions/Prompts

--- Assets/Scripts/BattleScripts/BattleMenuScript.cs
+++ Assets/Scripts/BattleScripts/BattleMenuScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using TMPro;
 
@@ -12,12 +14,12 @@ public class BattleMenuScript : MonoBehaviour
     //TheCanvas
     public GameObject battleButtonCanvas;
 
-    public GameObject playerHealthTextUI, playerSPTextUI, playerXPTextUI;
+    public GameObject playerHealthTextUI, playerSPTextUI, playerXPTextUI, playerAttackTitleTextUI, playerAttackDescTextUI, playerSpinTitleTextUI, playerSpinDescTextUI, playerItemTitleTextUI, playerItemDescTextUI;
     public GameObject player;
     private PlayerBattler playerBattlerScript;
     GameObject selectedOption, savedOption;
 
-    TextMeshProUGUI healthText, SPText, XPText;
+    TextMeshProUGUI healthText, SPText, XPText, attackTitle, attackDesc, spinTitle, spinDesc, itemTitle, itemDesc;
 
     void Start()
     {
@@ -25,6 +27,13 @@ public class BattleMenuScript : MonoBehaviour
         healthText = playerHealthTextUI.GetComponent<TextMeshProUGUI>();
         SPText = playerSPTextUI.GetComponent<TextMeshProUGUI>();
         XPText = playerXPTextUI.GetComponent<TextMeshProUGUI>();
+
+        attackTitle = playerAttackTitleTextUI.GetComponent<TextMeshProUGUI>();
+        attackDesc = playerAttackDescTextUI.GetComponent<TextMeshProUGUI>();
+        spinTitle = playerSpinTitleTextUI.GetComponent<TextMeshProUGUI>();
+        spinDesc = playerSpinDescTextUI.GetComponent<TextMeshProUGUI>();
+        itemTitle = playerItemTitleTextUI.GetComponent<TextMeshProUGUI>();
+        itemDesc = playerItemDescTextUI.GetComponent<TextMeshProUGUI>();
 
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(menuAttack);
@@ -41,9 +50,7 @@ public class BattleMenuScript : MonoBehaviour
         XPText.text = $"XP: {playerBattlerScript.xp}/100";  
         //Debug.Log("Selected game object:" + EventSystem.current.currentSelectedGameObject);
         selectedOption = EventSystem.current.currentSelectedGameObject;
-        //Debug.Log("Selected game object: " + selectedOption);
-
-        
+        //Debug.Log("Selected game object: " + selectedOption);       
     }
 
     void menuAnimator()
@@ -63,6 +70,9 @@ public class BattleMenuScript : MonoBehaviour
         savedOption = EventSystem.current.currentSelectedGameObject;
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(enemyAttackButton1);
+        
+        attackTitle.text = "Jump!";
+        attackDesc.text = "Jump on the enemy and press Z at the right time to jump again and deal even more damage!";
     }
 
     public void EnemySpinSelector()
@@ -70,11 +80,65 @@ public class BattleMenuScript : MonoBehaviour
         savedOption = EventSystem.current.currentSelectedGameObject;
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(enemySpinButton1);
+
+        spinTitle.text = "Spin!";
+        spinDesc.text = "Idk what to put here yet tbh...";
     }
 
     public void ReturnMenu()
     {
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(savedOption);
+    }
+
+    public void ItemDescriptions(int value)
+    {
+        switch (value)
+        {
+            case 1:
+                itemTitle.text = "Item Title 1";
+                itemDesc.text = "Item Description 1";
+                break;
+
+            case 2:
+                itemTitle.text = "Item Title 2";
+                itemDesc.text = "Item Description 2";
+                break;
+            
+            case 3:
+                itemTitle.text = "Item Title 3";
+                itemDesc.text = "Item Description 3";
+                break;
+            
+            case 4:
+                itemTitle.text = "Item Title 4";
+                itemDesc.text = "Item Description 4";
+                break;
+            
+            case 5:
+                itemTitle.text = "Item Title 5";
+                itemDesc.text = "Item Description 5";
+                break;
+            
+            case 6:
+                itemTitle.text = "Item Title 6";
+                itemDesc.text = "Item Description 6";
+                break;
+            
+            case 7:
+                itemTitle.text = "Item Title 7";
+                itemDesc.text = "Item Description 7";
+                break;
+            
+            case 8:
+                itemTitle.text = "Item Title 8";
+                itemDesc.text = "Item Description 8";
+                break;
+            
+            default:
+                itemTitle.text = "";
+                itemDesc.text = "Select an item.";
+                break;      
+        }
     }
 }
