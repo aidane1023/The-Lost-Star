d8f56afd8eae3d900d5e6e4c0940cad8ac2820ad
Abner-Wilhelm <122558294+Abner-Wilhelm@users.noreply.github.com>
2024-04-01 14:53:18 -0400

    Rah

--- Assets/Scripts/ItemScripts/podiumdisplay.cs
+++ Assets/Scripts/ItemScripts/podiumdisplay.cs
@@ -5,58 +5,60 @@ using UnityEngine;
 
 public class podiumdisplay : MonoBehaviour
 {
-
     public Database itemDatabase;
     public SpriteRenderer displaySprite;
     public float itemScale = 4;
-    private bool followPlayer = false;
-    private bool holding = false;
     public Transform objectToFollow;
     public Vector3 offset;
-   
-    public void Start()
+
+    private Vector3 startingPosition;
+    private bool holding = false;
+
+    private void Start()
     {
-        randomDisplay();
-        holding = false;
+        startingPosition = transform.position;
+        RandomDisplay();
     }
 
-    public void Update()
+    private void Update()
     {
-        if (followPlayer == true)
+        if (holding)
         {
             transform.position = objectToFollow.position + offset;
         }
     }
 
-    public void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "shop")
+        if (other.CompareTag("shop"))
         {
-            Debug.Log("follow player disabled");
-            followPlayer = false;
-            holding = false;
-            displaySprite.enabled = false;
-            return;
+            ReturnToOriginalPosition();
         }
-
-        if (other.tag == "Player" && holding == false)
-            {
-            Debug.Log("Space pressed");
-            followPlayer = true;
-            holding = true;
-            return;
-            }
-       
-
+        else if (other.CompareTag("Player") && !holding)
+        {
+            PickUpObject();
+        }
     }
 
-    void randomDisplay()
+    private void PickUpObject()
     {
-        if (itemDatabase == null || itemDatabase.Items.Count == 0) return;
+        holding = true;
+    }
+
+    private void ReturnToOriginalPosition()
+    {
+        holding = false;
+        transform.position = startingPosition;
+    }
+
+    private void RandomDisplay()
+    {
+        if (itemDatabase == null || itemDatabase.Items.Count == 0)
+            return;
 
         int randomID = Random.Range(0, itemDatabase.Items.Count);
         InventoryItemData randomItem = itemDatabase.GetItem(randomID);
         displaySprite.sprite = randomItem.Icon;
-        displaySprite.size = new Vector2 (itemScale, itemScale);
+        displaySprite.size = new Vector2(itemScale, itemScale);
     }
 }
