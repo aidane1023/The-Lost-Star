1360956bf37b71702ec65696f39c07473e95a2d3
aidane1023 <earnestaidan@gmail.com>
2024-04-08 17:39:57 -0400

    Fixed Waddle

--- Assets/Scripts/OverworldScripts/PlayerController.cs
+++ Assets/Scripts/OverworldScripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     public Animator animator;
+    private float movingX;
     private float moving;
+    private bool flip;
     public Rigidbody rb;
     public SpriteRenderer sr;
     
@@ -26,15 +28,18 @@ public class PlayerController : MonoBehaviour
 
         if (x != 0 && x < 0)
         {
+            flip = true;
             sr.flipX = true;
         }
         else if (x != 0 && x > 0)
         {
+            flip = false;
             sr.flipX = false;
         }
 
         moving = (x*x + y*y);
         animator.SetFloat("moving", moving);
+        animator.SetBool("flip", flip);
     }
 
     void RelocateAfterBattle()
