d390a07cd28c4a3a4f0bcd8738240d0386aa1530
aidane1023 <earnestaidan@gmail.com>
2024-04-10 23:55:14 -0400

    Moving Water

---
+++ Assets/Scripts/OverworldScripts/ScrollTexture.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public float scrollX = 0f;
+    public float scrollY = -0.5f;
+
+    void Update() 
+    {
+        float offsetX = Time.time * scrollX;
+        float offsetY = Time.time * scrollY;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
