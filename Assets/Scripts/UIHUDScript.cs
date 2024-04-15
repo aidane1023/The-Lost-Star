8a18e2a7233df85552aeb4c0cd4778f11db5e449
SolidZan <zanman263@gmail.com>
2024-04-15 02:51:37 -0400

    Player Stats Update Between Scenes
    Health, Sp, Xp and Coins. (Coins are not fully implemented yet)

--- Assets/Scripts/UIHUDScript.cs
+++ Assets/Scripts/UIHUDScript.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIHUDScript : MonoBehaviour
 {
-    //public GameObject playerHealthTextUI, playerSPTextUI, playerXPTextUI, playerCoinTextUI;
+    public GameObject playerHealthTextUI, playerSPTextUI, playerXPTextUI, playerCoinTextUI, xpBarUI;
     //public GameObject player;
 
-    //TextMeshProUGUI healthText, SPText, XPText, coinText;
+    TextMeshProUGUI healthText, SPText, XPText, coinText;
+    Image xpBarColorFill;
 
     //private PlayerStats playerStatsScript;
 
@@ -17,18 +19,20 @@ public class UIHUDScript : MonoBehaviour
     void Start()
     {
         //playerStatsScript = player.GetComponent<>();
-        //healthText = playerHealthTextUI.GetComponent<TextMeshProUGUI>();
-        //SPText = playerSPTextUI.GetComponent<TextMeshProUGUI>();
-        //XPText = playerXPTextUI.GetComponent<TextMeshProUGUI>();
+        healthText = playerHealthTextUI.GetComponent<TextMeshProUGUI>();
+        SPText = playerSPTextUI.GetComponent<TextMeshProUGUI>();
+        XPText = playerXPTextUI.GetComponent<TextMeshProUGUI>();
+        xpBarColorFill = xpBarUI.GetComponent<Image>();
         //coinText = playerCoinTextUI.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //healthText.text = $"Health: {playerStatsScript.health}/{playerStatsScript.maxHealth}";
-        //SPText.text = $"SP: {playerStatsScript.starPoints}/{playerStatsScript.maxStarPoints}";
-        //XPText.text = $"XP: {playerStatsScript.xp}/100";
+        healthText.text = $"HP: {PlayerBattler.health}/{PlayerBattler.maxHealth}";
+        SPText.text = $"SP: {PlayerBattler.starPoints}/{PlayerBattler.maxStarPoints}";
+        XPText.text = $"XP: {PlayerBattler.xp}/100";
+        xpBarColorFill.fillAmount = (PlayerBattler.xp/100);
         //coinText.text = $"Coins: {playerStatsScript.Coins}";
     }
 }
