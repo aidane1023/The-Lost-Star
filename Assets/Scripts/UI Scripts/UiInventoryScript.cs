2ec25b2586a79f713858d0a38bf9a91e44a629c9
SolidZan <zanman263@gmail.com>
2024-04-17 17:21:11 -0400

    Test Update

--- Assets/Scripts/UI Scripts/UiInventoryScript.cs
+++ Assets/Scripts/UI Scripts/UiInventoryScript.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class UiInventoryScript : MonoBehaviour
 {
+    public PlayerInventory inventory;
+
+    public string[] itemName;
+    //private string[] itemNameDisplay;
+    int itemNameLength;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log(inventory.heldItems[0].name);
+        itemNameLength = inventory.heldItems.Length;
+        //itemName.Length = new string[itemNameLength];
+        //itemName[0] = inventory[0].Name;
+        Debug.Log(itemNameLength);
     }
 
     // Update is called once per frame
