ebd55482fd7109300c2d277a1f4d530d6eb6a82f
aidane1023 <earnestaidan@gmail.com>
2024-04-21 10:20:27 -0400

    Revert "Merge branch 'Yet-Another-UI-Branch' of https://github.com/aidane1023/GP3_Group1 into Yet-Another-UI-Branch"
    This reverts commit 3e531bd306afb3ef55c91ed5e15a7427e40bd628, reversing
    changes made to 0b6fe5ae3e369eed19d14b5d70d6db4125726045.

--- Assets/Scripts/UI Scripts/UIMainMenu.cs
+++ Assets/Scripts/UI Scripts/UIMainMenu.cs
@@ -11,26 +11,14 @@ using UnityEngine.SceneManagement;
 public class UIMainMenu : MonoBehaviour
 {
 
-    public GameObject menuStart, menuControls, menuCredits, menuExit, defaultMain, defaultCredits, defaultControls, creditsMenu;
-    public GameObject[] credits;
-    int currentMenu;
-    private GameObject savedOption;
-    private Button startButton, controlsButton, creditsButton, exitButton;
+    public GameObject menuStart, menuControls, menuCredits, menuExit, defaultMain, defaultCredits, defaultControls;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        startButton = menuStart.GetComponent<Button>();
-        controlsButton = menuControls.GetComponent<Button>();
-        creditsButton = menuCredits.GetComponent<Button>();
-        exitButton = menuExit.GetComponent<Button>();
-
-        currentMenu = 0;
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(menuStart);
-
     }
 
     // Update is called once per frame
@@ -38,53 +26,13 @@ public class UIMainMenu : MonoBehaviour
     {
         if (EventSystem.current.currentSelectedGameObject == null)
         {
-            switch (currentMenu)
-            {
-                case 0:
-                EventSystem.current.SetSelectedGameObject(menuStart);
-                break;
-
-                case 1:
-                EventSystem.current.SetSelectedGameObject(defaultCredits);
-                break;
-
-                default:
-                break;
-            }
-        }
-
-        if (Input.GetButtonDown("Cancel"))
-        {
-            if (currentMenu == 1);
-            {
-                creditsMenu.SetActive(false);
-                ReturnMenu(savedOption);
-                Debug.Log(savedOption);
-                currentMenu = 0;
-            }
+            EventSystem.current.SetSelectedGameObject(menuStart);
         }
     }
 
-    public void ReturnMenu(GameObject returnOption)
+    public void StartGame()
     {
-        if (currentMenu == 1)
-        {
-            startButton.interactable = true;
-            controlsButton.interactable = true;
-            creditsButton.interactable = true;
-            exitButton.interactable = true;   
-        }
-        
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(returnOption);
-    }
 
-    public void OpenCredits()
-    {
-        currentMenu = 1;
-        savedOption = menuCredits;
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(defaultCredits);
     }
 
     public void ExitGame()
