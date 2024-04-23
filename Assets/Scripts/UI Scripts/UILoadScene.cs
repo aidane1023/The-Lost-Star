f715c66c0f2b87ac5b5b999fdb6a15d00cfbf2c7
SolidZan <144177149+SolidZan@users.noreply.github.com>
2024-04-23 12:13:12 -0400

    Fixed Loading Screen

--- Assets/Scripts/UI Scripts/UILoadScene.cs
+++ Assets/Scripts/UI Scripts/UILoadScene.cs
@@ -20,6 +20,7 @@ public class UILoadScene : MonoBehaviour
     IEnumerator LoadSceneAsync(int sceneID)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        operation.allowSceneActivation = false;
         
         loadingScreen.SetActive(true);
 
@@ -28,7 +29,13 @@ public class UILoadScene : MonoBehaviour
             float progressValue = Mathf.Clamp01(operation.progress/ 0.9f);
 
             loadBar.fillAmount = progressValue;
-            loadProgressText.text = $"{Mathf.Round(progressValue/100)}";
+            loadProgressText.text = $"{progressValue * 100}%";
+            Debug.Log(progressValue * 100);
+
+            if (operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
 
             yield return null;
         }
