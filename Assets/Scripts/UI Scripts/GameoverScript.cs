09412d7fa49b4b9929982881fc0292829355a0d5
SolidZan <zanman263@gmail.com>
2024-04-26 22:25:31 -0400

    GameOver Screen Added

---
+++ Assets/Scripts/UI Scripts/GameoverScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameoverScript : MonoBehaviour
+{
+
+    public GameObject continueButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(continueButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null) EventSystem.current.SetSelectedGameObject(continueButton);
+    }
+
+    public void Continue()
+    {
+        PlayerBattler.health = PlayerBattler.maxHealth;
+        PlayerBattler.starPoints = PlayerBattler.maxStarPoints;
+        SceneManager.LoadScene("HUBBuild");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game Exited");
+    }
+}
