f09a44e5483d7e33cfe5f1e33b8f4d0a7c5c8d0b
Amanda Moller <107587785+musicDynamo@users.noreply.github.com>
2024-04-27 01:29:35 -0400

    StoreSFX partone

--- Assets/Scripts/ItemScripts/ItemDisplay.cs
+++ Assets/Scripts/ItemScripts/ItemDisplay.cs
@@ -31,6 +31,11 @@ public class ItemDisplay : MonoBehaviour
     private static List<ItemDisplay> pickedItems = new List<ItemDisplay>(); // List to track picked up items
     private static bool IsHoldingItem;
 
+    public AudioSource source;
+    public AudioClip pickUp;
+    public AudioClip storeCheckout;
+    public AudioClip noMoney;
+
     private void Start()
     {
         startingPosition = transform.position;
@@ -55,6 +60,7 @@ public class ItemDisplay : MonoBehaviour
             PickUpObject();
             shopUI.SetActive(false);
             IsHoldingItem=true;
+            source.PlayOneShot(pickUp);
         }
 
         if (holding)
