e2f15c97a65f65e14dd2b6e62ed5dda5497e4ed5
aidane1023 <earnestaidan@gmail.com>
2024-04-27 23:37:38 -0400

    Solider and Jack Intro

--- Assets/Scripts/OverworldScripts/EnemyChase.cs
+++ Assets/Scripts/OverworldScripts/EnemyChase.cs
@@ -10,6 +10,9 @@ public class EnemyChase : MonoBehaviour
     bool canStartBattle = false;
 
     public float speed;
+    public float range;
+    public bool soldier;
+    public Animator anim;
 
     void Awake()
     {
@@ -19,10 +22,11 @@ public class EnemyChase : MonoBehaviour
 
     void Update()
     {
-        if ((Vector3.Distance(player.position, this.transform.position) <= 3f))
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
+            if ((Vector3.Distance(player.position, this.transform.position) <= range))
+            {
+                if (soldier) StartCoroutine(DelayJump());
+                transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
 
         if ((Vector3.Distance(player.position, this.transform.position) <= 0.4f) && canStartBattle)
         {
@@ -35,5 +39,20 @@ public class EnemyChase : MonoBehaviour
     {
         yield return new WaitForSeconds(3);
         canStartBattle = true;
+    }
+
+    void OnTriggerEnter()
+    {
+        anim.SetBool("InRange", true);
+    }
+
+    IEnumerator DelayJump()
+    {
+        speed = 0;
+        yield return new WaitForSeconds(0.25f);
+        speed = 4;
+        yield return new WaitForSeconds(0.5f);
+        speed = 0;
+        yield return new WaitForSeconds(0.25f);
     }
 }
