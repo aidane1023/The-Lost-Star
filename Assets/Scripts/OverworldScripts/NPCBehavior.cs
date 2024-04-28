e212c314a0900ba07f330afdd1661b7065923e1e
SolidZan <zanman263@gmail.com>
2024-04-28 00:21:26 -0400

    Dialogue Set Up Now

--- Assets/Scripts/OverworldScripts/NPCBehavior.cs
+++ Assets/Scripts/OverworldScripts/NPCBehavior.cs
@@ -7,29 +7,27 @@ public class NPCBehavior : MonoBehaviour
     public GameObject player, characterDialogue;
     PlayerController playerController;
     public GameObject[] dialogueObjects, interactObjects;
-    private Dialogue dialogue;
+    public Dialogue[] dialogue;
     bool textPlaying = false;
     public Animator playerAnimator;
     public int dialogueCount;
     int index = 0;
     float timer = 0f;
+    public bool isMozzy, isBouncer, isLegoGuy;
+    public Level3Items itemManager;
 
     public AudioSource source;
     public AudioClip dialoguePress;
     
     void Start()
     {
-        //for (int i = 0; i < dialogueCount; i++)
-        //{
-        dialogue = dialogueObjects[0].GetComponent<Dialogue>();      
-        //}
         playerController = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if ((Vector3.Distance(player.transform.position, this.transform.position) <= 3f) && Input.GetKeyUp(KeyCode.Z) && !textPlaying)
+    {       
+        if ((Vector3.Distance(player.transform.position, this.transform.position) <= 3f) && Input.GetButtonDown("Submit") && !textPlaying)
         {
             characterDialogue.SetActive(true);
             playerAnimator.SetFloat("moving", 0);
@@ -37,37 +35,94 @@ public class NPCBehavior : MonoBehaviour
             player.GetComponent<AudioSource>().enabled = false;
             player.GetComponent<PlayerController>().enabled = false;
             textPlaying = true;
-            dialogueObjects[0].SetActive(true);
+
+            if (isMozzy && itemManager.watchFound && !itemManager.watchGiven)
+            {
+                index = 2;
+            }
+
+            if (isLegoGuy && itemManager.lighterFound && !itemManager.lighterGiven)
+            {
+                index = 1;
+            }
+
+            if (isBouncer && itemManager.ticketFound && !itemManager.ticketGiven)
+            {
+                index = 1;
+            }
+
+            dialogueObjects[index].SetActive(true);
             interactObjects[0].SetActive(false);
-            dialogue.DialogueTriggered();
+            dialogue[index].DialogueTriggered();
 
             source.PlayOneShot(dialoguePress);
         }
-        if ((dialogue.dialogueEnded == true) && (textPlaying == true))
+        if (dialogue[index] != null)
         {
-            player.GetComponent<PlayerController>().enabled = true;
-            player.GetComponent<AudioSource>().enabled = true;
-            playerController.speed = 4f;
-            characterDialogue.SetActive(false);
-            if (timer < 2.0f)
+            if ((dialogue[index].dialogueEnded == true) && (textPlaying == true))
             {
-                timer += Time.deltaTime;
+                player.GetComponent<PlayerController>().enabled = true;
+                player.GetComponent<AudioSource>().enabled = true;
+                playerController.speed = 4f;
+                characterDialogue.SetActive(false);
+                if (timer < 2.0f)
+                {
+                    timer += Time.deltaTime;
+                }
+                else
+                {
+                    Debug.Log("Dialogue ended.");
+                    //index = 1;
+                    
+                    timer = 0f;
+                    interactObjects[0].SetActive(true);
+                    textPlaying = false;
+                    //Reset();
+                    Debug.Log(index);
+                    if (isMozzy && index == 0)
+                    {
+                        index = 1;
+                    }
+                }
             }
-            else
-            {
-                Debug.Log("Dialogue ended.");
-                //index = 2;
-                
-                timer = 0f;
-                interactObjects[0].SetActive(true);
-                textPlaying = false;
-                //Reset();
-            }
-
-        }
-        
+        }   
+    }
+    public void ShutUp()
+    {
+        player.GetComponent<PlayerController>().enabled = true;
+        player.GetComponent<AudioSource>().enabled = true;
+        playerController.speed = 4f;
+        dialogueObjects[0].SetActive(false);
+        this.enabled = false;
     }
 
+    public void giveWatch()
+    {
+        itemManager.watchGiven = true;
+        index = 3;
+    }
+
+    public void getLighter()
+    {
+        itemManager.lighterFound = true;
+    }
+
+    public void getTicket()
+    {
+        itemManager.ticketFound = true;
+    }
+
+    public void giveLighter()
+    {
+        itemManager.lighterGiven = true;
+        index = 2;
+    }
+
+    public void giveTicket()
+    {
+        itemManager.ticketGiven = true;
+        index = 2;
+    }
     //IEnumerator Reset()
     //{
     //    yield return new WaitForSeconds(2f);
