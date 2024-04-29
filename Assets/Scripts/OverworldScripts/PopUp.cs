ac9efb4f63e25d7c1a6f5e80910a6f347c5e1237
Cameron Rutkowski <moatsofgoats@gmail.com>
2024-04-28 21:59:55 -0400

    Cant Go Back

--- Assets/Scripts/OverworldScripts/PopUp.cs
+++ Assets/Scripts/OverworldScripts/PopUp.cs
@@ -9,10 +9,20 @@ public class PopUp : MonoBehaviour
     public Transform player;
     public bool movingUp = false;
     public AudioSource source;
+
+    public int pieceInArea = 0; //if the rocket piece is obtained from here, disable the popup
+    bool interactable = true;
+
+    void Start()
+    {
+        if(GameManager.Instance.HasTop && pieceInArea == 1) interactable = false;
+        if(GameManager.Instance.HasMiddle && pieceInArea == 2) interactable = false;
+        if(GameManager.Instance.HasBottom && pieceInArea == 3) interactable = false;
+    }
     
     void Update()
     {
-        if (TrainingDummy.cleared)
+        if (TrainingDummy.cleared && interactable)
         {
             if ((Vector3.Distance(player.position, this.transform.position) <= 3.3f))
             {
